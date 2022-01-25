5a08fdbc468b16b2b27d1cad257d9f434584eb14
Kuba <jakubkozlowski.pl@gmail.com>
2022-01-25 17:14:08 +0100

    Added styled-components and GlobalStyle.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
-import React from "react"
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  background-color: red;
+`
 
 const Home = () => {
-  return <h1>Hello</h1>
+  return <Wrapper>Witam</Wrapper>
 }
 
 export default Home
