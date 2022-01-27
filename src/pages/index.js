1c6f9310d8cb530241911a3b94d28dcf619a4e5b
Kuba <jakubkozlowski.pl@gmail.com>
2022-01-27 12:44:38 +0100

    Created toggle menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GlobalStyle from './styles/GlobalStyle'
 import styled from 'styled-components'
 import { StyledLogo } from './../components/atoms/Logo/Logo'
-import MySvg from './../../assets/icons/ClosedMenuIcon.svg'
+import ClosedMenuIcon from './../../assets/icons/ClosedMenuIcon.svg'
+import OpenedMenuIcon from './../../assets/icons/OpenedMenuIcon.svg'
 
 const Container = styled.div`
   width: 100%;
   padding: 30px 50px;
+  height: 100%;
 `
 
-const NavMenu = styled.nav`
+const Header = styled.nav`
   display: flex;
   justify-content: space-between;
+  width: 100%;
+  height: 100%;
 `
 
+const Menu = styled.ul``
+
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <>
       <GlobalStyle />
       <Container>
-        <NavMenu>
+        <Header>
           <StyledLogo>logo.</StyledLogo>
-          <img src={MySvg} />
-        </NavMenu>
+
+          <div onClick={() => setIsOpen(!isOpen)}>
+            <img src={isOpen ? OpenedMenuIcon : ClosedMenuIcon} />
+          </div>
+        </Header>
+        {isOpen ? null : (
+          <Menu>
+            <li>o mnie</li>
+            <li>usługi</li>
+            <li>realizacje</li>
+            <li>kontakt</li>
+          </Menu>
+        )}
       </Container>
     </>
   )
