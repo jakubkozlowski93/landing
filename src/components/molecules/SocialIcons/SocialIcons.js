5eed7bfc48a19de7bb63e6bffb74902e70e9c1d5
Kuba <jakubkozlowski.pl@gmail.com>
2022-02-08 22:34:24 +0100

    Moved footer components to other files

---
+++ src/components/molecules/SocialIcons/SocialIcons.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import styled from 'styled-components'
+import FacebookIcon from './../../../assets/icons/FacebookIcon.svg'
+import LinkedInIcon from './../../../assets/icons/LinkedInIcon.svg'
+import InstagramIcon from './../../../assets/icons/InstagramIcon.svg'
+import { motion } from 'framer-motion'
+
+const Wrapper = styled.footer`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  ul {
+    list-style-type: none;
+    display: flex;
+
+    li {
+      padding: 0 20px;
+      width: 100%;
+      color: #242635;
+    }
+  }
+`
+
+const SocialIcons = () => {
+  return (
+    <motion.div initial={{ y: '100vh' }} animate={{ y: 0 }} transition={{ type: 'spring', stiffness: 60, delay: 0.2 }}>
+      <Wrapper>
+        <ul>
+          <li>
+            <img src={FacebookIcon} alt="Facebook icon" />
+          </li>
+          <li>
+            <img src={InstagramIcon} alt="Instagram Icon" />
+          </li>
+          <li>
+            <img src={LinkedInIcon} alt="LinkedIn icon" />
+          </li>
+        </ul>
+      </Wrapper>
+    </motion.div>
+  )
+}
+
+export default SocialIcons
