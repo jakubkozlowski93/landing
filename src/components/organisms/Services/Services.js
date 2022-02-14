f6a59610b4adb3c54dfc47326d54225e22fc9672
Kuba <jakubkozlowski.pl@gmail.com>
2022-02-14 21:46:26 +0100

    Moved styles in component services to another file

--- src/components/organisms/Services/Services.js
+++ src/components/organisms/Services/Services.js
@@ -1,67 +1,9 @@
 import React, { useEffect } from 'react'
-import styled from 'styled-components'
+import { Wrapper, Container, SingleService, StyledTitle, StyledInfo } from './Services.styles'
 import Bar from './../../atoms/Bar/Bar'
 import ServiceIcon from './../../../assets/icons/ServiceIcon.svg'
 import { motion, useAnimation } from 'framer-motion'
 import { useInView } from 'react-intersection-observer'
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin: 0 auto;
-`
-
-const Container = styled.div`
-  padding: 0 50px;
-  @media (min-width: 1000px) {
-    display: grid;
-    grid-template-columns: 50% 50%;
-    grid-template-rows: 2;
-    grid-auto-flow: row;
-  }
-`
-
-const SingleService = styled.div`
-  width: 100%;
-  position: relative;
-  margin: 20px;
-  padding: 25px 0;
-
-  img {
-    position: absolute;
-  }
-
-  @media (min-width: 1000px) {
-    width: 400px;
-  }
-`
-
-const StyledTitle = styled.h2`
-  font-size: 16px;
-  line-height: 20px;
-  text-transform: uppercase;
-  font-weight: bold;
-  letter-spacing: 0.18538em;
-  margin-left: 20px;
-
-  @media (min-width: 850px) {
-    font-size: 20px;
-  }
-`
-
-const StyledInfo = styled.p`
-  text-align: justify;
-  font-weight: 300;
-  padding-left: 40px;
-  font-size: 12px;
-  line-height: 16px;
-
-  @media (min-width: 850px) {
-    font-size: 14px;
-  }
-`
 
 const Services = () => {
   const [isFinished, setIsFinished] = React.useState(false)
