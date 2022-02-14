bb755073368060ef2d4f96980cbfa0b6dede8179
Kuba <jakubkozlowski.pl@gmail.com>
2022-02-14 21:36:20 +0100

    Added media query in component services

--- src/components/organisms/Services/Services.js
+++ src/components/organisms/Services/Services.js
@@ -10,16 +10,31 @@ const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 50px;
+  margin: 0 auto;
+`
+
+const Container = styled.div`
+  padding: 0 50px;
+  @media (min-width: 1000px) {
+    display: grid;
+    grid-template-columns: 50% 50%;
+    grid-template-rows: 2;
+    grid-auto-flow: row;
+  }
 `
 
 const SingleService = styled.div`
   width: 100%;
   position: relative;
-  margin: 20px 0;
+  margin: 20px;
+  padding: 25px 0;
 
   img {
     position: absolute;
+  }
+
+  @media (min-width: 1000px) {
+    width: 400px;
   }
 `
 
@@ -29,7 +44,11 @@ const StyledTitle = styled.h2`
   text-transform: uppercase;
   font-weight: bold;
   letter-spacing: 0.18538em;
-  margin-left: 20px; ;
+  margin-left: 20px;
+
+  @media (min-width: 850px) {
+    font-size: 20px;
+  }
 `
 
 const StyledInfo = styled.p`
@@ -38,6 +57,10 @@ const StyledInfo = styled.p`
   padding-left: 40px;
   font-size: 12px;
   line-height: 16px;
+
+  @media (min-width: 850px) {
+    font-size: 14px;
+  }
 `
 
 const Services = () => {
@@ -67,49 +90,51 @@ const Services = () => {
   return (
     <Wrapper ref={ref}>
       <Bar text="Usługi" />
-      <motion.div custom={0} animate={animationFromLeft}>
-        <SingleService>
-          <img src={ServiceIcon} alt="Service icon" />
-          <StyledTitle>projekt</StyledTitle>
-          <StyledInfo>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, expedita ab aliquam atque consequuntur id possimus, eum quod incidunt
-            corrupti quia iure dicta.
-          </StyledInfo>
-        </SingleService>
-      </motion.div>
+      <Container>
+        <motion.div custom={0} animate={animationFromLeft}>
+          <SingleService>
+            <img src={ServiceIcon} alt="Service icon" />
+            <StyledTitle>projekt</StyledTitle>
+            <StyledInfo>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, expedita ab aliquam atque consequuntur id possimus, eum quod incidunt
+              corrupti quia iure dicta.
+            </StyledInfo>
+          </SingleService>
+        </motion.div>
 
-      <motion.div custom={1} animate={animationFromLeft}>
-        <SingleService>
-          <img src={ServiceIcon} alt="Service icon" />
-          <StyledTitle>projekt</StyledTitle>
-          <StyledInfo>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, expedita ab aliquam atque consequuntur id possimus, eum quod incidunt
-            corrupti quia iure dicta.
-          </StyledInfo>
-        </SingleService>
-      </motion.div>
+        <motion.div custom={1} animate={animationFromLeft}>
+          <SingleService>
+            <img src={ServiceIcon} alt="Service icon" />
+            <StyledTitle>projekt</StyledTitle>
+            <StyledInfo>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, expedita ab aliquam atque consequuntur id possimus, eum quod incidunt
+              corrupti quia iure dicta.
+            </StyledInfo>
+          </SingleService>
+        </motion.div>
 
-      <motion.div custom={2} animate={animationFromLeft}>
-        <SingleService>
-          <img src={ServiceIcon} alt="Service icon" />
-          <StyledTitle>projekt</StyledTitle>
-          <StyledInfo>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, expedita ab aliquam atque consequuntur id possimus, eum quod incidunt
-            corrupti quia iure dicta.
-          </StyledInfo>
-        </SingleService>
-      </motion.div>
+        <motion.div custom={2} animate={animationFromLeft}>
+          <SingleService>
+            <img src={ServiceIcon} alt="Service icon" />
+            <StyledTitle>projekt</StyledTitle>
+            <StyledInfo>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, expedita ab aliquam atque consequuntur id possimus, eum quod incidunt
+              corrupti quia iure dicta.
+            </StyledInfo>
+          </SingleService>
+        </motion.div>
 
-      <motion.div custom={3} animate={animationFromLeft}>
-        <SingleService>
-          <img src={ServiceIcon} alt="Service icon" />
-          <StyledTitle>projekt</StyledTitle>
-          <StyledInfo>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, expedita ab aliquam atque consequuntur id possimus, eum quod incidunt
-            corrupti quia iure dicta.
-          </StyledInfo>
-        </SingleService>
-      </motion.div>
+        <motion.div custom={3} animate={animationFromLeft}>
+          <SingleService>
+            <img src={ServiceIcon} alt="Service icon" />
+            <StyledTitle>projekt</StyledTitle>
+            <StyledInfo>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa, expedita ab aliquam atque consequuntur id possimus, eum quod incidunt
+              corrupti quia iure dicta.
+            </StyledInfo>
+          </SingleService>
+        </motion.div>
+      </Container>
     </Wrapper>
   )
 }
